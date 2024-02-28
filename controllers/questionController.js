2011a5d610ac9c7d682605d5d30e70d3e04c8a53
anchellon <anshulmathew@gmail.com>
2024-02-28 13:24:14 -0800

    Question addition works

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -1,6 +1,6 @@
 const Quiz = require("../model/Quiz");
 const Question = require("../model/Question");
-const { body, validationResult } = require("express-validator");
+// const { body, validationResult } = require("express-validator");
 exports.question_info_getMethod = async (req, res) => {
     let qns = await Question.find({ quiz: req.params.qzid });
     console.log(req.params.qzid);
@@ -11,3 +11,18 @@ exports.question_info_getMethod = async (req, res) => {
 exports.question_deleteMethod = (req, res) => {
     res.send("NOT IMPLEMENTED: question delete method");
 };
+
+exports.question_create_postMethod = [
+    async (req, res, next) => {
+        console.log(req.body);
+        // let options = req.body.options;
+        // let correctAnswer = req.body.isCorrect;
+        let qn = await Question.create({
+            qnText: req.body.questionText,
+            quiz: req.body.quiz,
+            options: req.body.options,
+            correctAnswer: req.body.correctAnswer,
+        });
+        res.status(200).send(qn);
+    },
+];
--- routes/api/question.js
+++ routes/api/question.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const question_controller = require("../../controllers/questionController");
 
 router.get("/qz/:qzid/", question_controller.question_info_getMethod);
-// router.post("/qz/:qzid", question_controller.question_create_postMethod);
+router.post("/qz/:qzid", question_controller.question_create_postMethod);
 
 router.delete("/:id", question_controller.question_deleteMethod);
 // POST Question details along with all options associcated with it
